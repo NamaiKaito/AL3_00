d988dde21410d5ff16f17a43e5846ddd21e9e39a
Kaito_Namai <k024g1004@g.neec.ac.jp>
2025-04-21 10:18:46 +0900

    コミット

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,7 +1,20 @@
 #include "GameScene.h"
+using namespace KamataEngine;
 
-void GameScene::Initialize() {}
+GameScene::~GameScene() { delete model_; }
+
+void GameScene::Initialize() {
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	camera_.Initialize();
+}
 
 void GameScene::Update() {}
 
-void GameScene::Draw() {}
+void GameScene::Draw() {
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	Model::PreDraw(dxCommon->GetCommandList());
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+	Model::PostDraw();
+}
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -3,7 +3,14 @@
 //ゲームシーン
 class GameScene 
 {
+private:
+	uint32_t textureHandle_ = 0;
+	KamataEngine::Model* model_ = nullptr;
+	KamataEngine::WorldTransform worldTransform_;
+	KamataEngine::Camera camera_;
+
 public:
+	~GameScene();
 	//初期化
 	void Initialize();
 	//更新
